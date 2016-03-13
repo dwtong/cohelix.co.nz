bd6dc0397d850d7dedebef142c801cdb304f804d
Dan Tong <danieltongnz@gmail.com>
2016-03-14 08:38:07 +1300

    Move assets to _app folder, start configuring gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var shell = require('gulp-shell');
+
+// pass -- drafts flag to serve drafts
+var config = {
+  drafts: !!gutil.env.drafts
+};
+
+// paths to various directories
+var paths = {
+  appDir: './_app',
+  appSassFiles: './_app/sass',
+  jekyllDir: './',
+  siteDir: './_site'
+};
+
+gulp.task('build:styles', function () {
+  return gulp.src(paths.appSassFiles + '/main.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9']}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(paths.jekyllDir))
+    .pipe(gulp.dest(paths.siteDir))
+    .pipe(browserSync.stream())
+    .on('error', gutil.log);
+});
 
 // Run Jekyll 'build' task, and watch for changes
 gulp.task('build', shell.task(['jekyll build --watch']));
